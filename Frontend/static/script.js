e1f1325147087bb43cb1451bf6117f4277482d69
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2025-06-03 14:58:52 +0200

    FIXED: Fixed the issue of enter not working when submitting a message

--- Frontend/static/script.js
+++ Frontend/static/script.js
@@ -1,20 +1,23 @@
 const socket = io();
 const username = document.getElementById("usernames")?.dataset.username;
-localStorage.setItem("usernameplayer", username);
 
-function SubmitMessage() {
+// Takes the information from frontend as a form that gets submited to backend
+document.getElementById("chat-form").addEventListener("submit", (e) => {
+    e.preventDefault()
     console.log("A message was sent")
     let message = document.getElementById("message").value;
-        let user_message = username + ": " + message
-        socket.emit('new_message', { user_message });
-        document.getElementById("message").value = "";
-}
+    let user_message = username + ": " + message
+    socket.emit('new_message', { user_message });
+    document.getElementById("message").value = "";
+})
 
-socket.on("chat", function(data) {
+
+// Takes the message from backend and displayes on frontend
+socket.on("chat", function (data) {
     let ul = document.getElementById("chat-messages");
     let li = document.createElement("li");
     console.log(data["user_message"]);
     li.appendChild(document.createTextNode(data["user_message"]));
     ul.appendChild(li);
-    ul.scrollBottom = ul.scrollHeight;SubmitSentence
+    ul.scrollBottom = ul.scrollHeight; SubmitSentence
 });
